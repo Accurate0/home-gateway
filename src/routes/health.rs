f9cfcadb3f2d9819442e0a5546b5e3b0c57dc0ae
Anurag Singh <contact@anurag.sh>
2025-12-27 10:16:37 +0800

    remove db check

--- src/routes/health.rs
+++ src/routes/health.rs
@@ -1,20 +1,7 @@
 use crate::types::ApiState;
 use axum::extract::State;
 use http::StatusCode;
-use sqlx::Connection;
 
-pub async fn health(State(ApiState { ref db, .. }): State<ApiState>) -> StatusCode {
-    let resp = db.acquire().await;
-
-    if resp.is_err() {
-        return StatusCode::SERVICE_UNAVAILABLE;
-    }
-
-    let is_db_ok = resp.unwrap().ping().await.is_ok();
-
-    if is_db_ok {
-        StatusCode::NO_CONTENT
-    } else {
-        StatusCode::SERVICE_UNAVAILABLE
-    }
+pub async fn health(State(ApiState { .. }): State<ApiState>) -> StatusCode {
+    StatusCode::NO_CONTENT
 }
