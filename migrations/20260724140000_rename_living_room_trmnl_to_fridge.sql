c04a44381b8ff804f4ed6b3076856f858aafa731
Anurag <contact@anurag.sh>
2026-07-24 22:36:43 +0800

    fix again

--- migrations/20260724140000_rename_living_room_trmnl_to_fridge.sql
+++ migrations/20260724140000_rename_living_room_trmnl_to_fridge.sql
@@ -1,3 +1,7 @@
+DELETE FROM eink_display
+WHERE device_id = 'fridge-trmnl'
+  AND EXISTS (SELECT 1 FROM eink_display WHERE device_id = 'living-room-trmnl');
+
 UPDATE eink_display SET device_id = 'fridge-trmnl', name = 'Fridge TRMNL' WHERE device_id = 'living-room-trmnl';
 
 UPDATE device_battery SET device_id = 'fridge-trmnl' WHERE device_id = 'living-room-trmnl';
